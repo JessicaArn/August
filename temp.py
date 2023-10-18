244c4e73e1754ad96b445bc15782021ad5f20be5
SunChinook <147000956+JessicaArn@users.noreply.github.com>
2023-10-18 15:27:11 -0600

    refactor code to make use of another function

--- temp.py
+++ temp.py
@@ -2,7 +2,15 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius*9/5+32
     return fahrenheit
 
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = fahrenheit-32*5/9
+    return celsius
+
 celsius = 25
 #fahrenheit = celsius*9/5+32
 fahrenheit = celsius_to_fahrenheit(celsius)
 print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+
+"given a fahrenheit value return the converted celsius value"
+fahrenheit = fahrenheit_to_celsius(fahrenheit)
+print(f"{fahrenheit} Fahrenheit to Celsius is {celsius}")
